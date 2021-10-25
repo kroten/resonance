050a3bee8af32f294afdf3b828f1fb2aa2f1a7d4
viktoriya <kroten@bk.ru>
2021-10-25 15:23:55 +0300

    second
    UI

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 
+#include <QtCharts/QChartView>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,47 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtCharts>
+#include <QtCharts/QChartView>
+
+#include "qgridlayout.h"
+#include <QGridLayout>
+#include "chartview.h"
+
+QList<QChart*> myChartList;
+QList<ChartView*> myChartViewList;
+QList<QSplineSeries*> mySplineSeriesList;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    for (int i = 0; i < 8; i++){
+        QSplineSeries *series = new QSplineSeries();
+        series->setName("spline");
+
+        QChart *chart1 = new QChart();
+
+        chart1->legend()->hide();
+        chart1->addSeries(series);
+        chart1->setTitle("Wire №" + QString::number(i+1));
+        chart1->createDefaultAxes();
+        chart1->axes(Qt::Vertical).first()->setRange(0, 20);
+        chart1->axes(Qt::Horizontal).first()->setRange(100, 300);
+
+        ChartView *chartView = new ChartView(chart1);
+
+        chartView->setRenderHint(QPainter::Antialiasing);
+        chartView->setMinimumSize(300, 150);
+        chartView->setMaximumSize(1000, 1000);
+
+        ui->gridLayout->addWidget(chartView, int(i/2), int(i%2));
+
+        mySplineSeriesList.append(series);
+        myChartList.append(chart1);
+        myChartViewList.append(chartView);
+    }
 }
 
 MainWindow::~MainWindow()
